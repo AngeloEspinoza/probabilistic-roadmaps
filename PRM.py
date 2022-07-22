55425e072eb73ca7ef8bfad81e6150687ea5ba12
Angelo <angelodavid98@hotmail.com>
2022-07-22 16:51:53 -0500

    Graph connecting without checking collision

--- PRM.py
+++ PRM.py
@@ -5,13 +5,23 @@ import graph
 # Constants
 MAP_DIMENSIONS = 640, 480
 
-# Instantiating the environment
+# Initial and final position of the robots
+x_init = 50, 50
+x_goal = 540, 380
+
+# Instantiating the environment and the graph
 environment_ = environment.Environment(dimensions=MAP_DIMENSIONS)
+graph_ = graph.Graph(start=x_init, goal=x_goal, 
+		map_dimensions=MAP_DIMENSIONS)
 
 def main():
 	run = True
 	clock = pygame.time.Clock()
 	obstacles = environment_.draw_obstacles()
+	configurations = []
+
+	n = 0 # Number of nodes to put in the roadmap
+	k = 3 # Number of the closest neighbors to examine for each configuration
 
 	while run:
 		# Make sure the loop runs at 60 FPS
@@ -20,7 +30,28 @@ def main():
 			if event.type == pygame.QUIT:
 				run = False
 
+		x_rand = graph_.generate_random_node()
+		collision_free = graph_.is_free(point=x_rand, obstacles=obstacles)
+		sampling = n <= graph_.MAX_NODES # Sampling time
+
+		if collision_free and sampling:
+			graph_.draw_random_node(map_=environment_.map)
+			configurations.append(x_rand)
+
+		if not sampling:
+			for configuration in configurations:
+				# Remove the current configuration 
+				new_configurations = configurations.copy()
+				new_configurations.remove(configuration)
+				x_near = graph_.nearest_neighbor(graph=new_configurations, 
+					x_rand=configuration, k=k)
+				for i in range(k):
+					pygame.draw.line(surface=environment_.map, color=(0, 0, 255)
+						, start_pos=configuration, end_pos=x_near[i])
+
+
+		n += 1 # Counter for the maximum allowed nodes		
 		pygame.display.update()
-		
+
 if __name__ == '__main__':
 	main()
---
+++ graph.py
@@ -0,0 +1,144 @@
+import pygame
+import random
+import math
+import numpy as np
+
+class Graph():
+	"""
+	A class for the Rapidly-Exploring Random Tree (RRT).
+	
+	Attributes
+	----------
+	start : tuple
+		Initial position of the tree in X and Y respectively.
+	goal : tuple
+		End position of the tree in X and Y respectively.
+	map_dimensions : tuple
+		Map width and height in pixels.
+	"""
+
+	def __init__(self, start, goal, map_dimensions):
+		self.x_init = start
+		self.x_goal = goal
+
+		self.WIDTH, self.HEIGHT = map_dimensions
+
+		self.MAX_NODES = 10
+		# Colors 
+		self.WHITE = (255, 255, 255)
+		self.BLACK = (0, 0, 0)
+		self.RED = (255, 0, 0)
+		self.GREEN = (0, 255, 0)
+		self.BLUE = (0, 0, 255)
+		self.BROWN = (189, 154, 122)
+		self.YELLOW = (255, 255, 0)
+
+	def is_free(self, point, obstacles, tree=None):
+		"""Checks whether a node is colliding with an obstacle or not.
+
+		When dealing with obstacles it is necessary to check 
+		for the collision with them from the generated node.
+
+		Parameters
+		----------
+		point : tuple
+			Point to be checked.
+		obstacles : pygame.Rect
+			Rectangle obstacle.
+		tree : list
+			Tree containing all the coordinate nodes.
+
+		Returns
+		-------
+		bool
+		"""
+		for obstacle in obstacles:
+			if obstacle.collidepoint(point):
+				# tree.remove(point)
+				return False
+
+		return True
+
+	def generate_random_node(self):
+		"""Generates a random node on the screen.
+
+		The x and y coordinate is generated given an uniform
+		distribution of the size of the screen width and height.
+
+		Parameters
+		----------
+		None
+
+		Returns
+		-------
+		tuple
+			Coordinates of the random node. 
+		"""
+		x, y = random.uniform(0, self.WIDTH),\
+		random.uniform(0, self.HEIGHT)
+		self.x_rand = x, y # To use within the class
+
+		return x, y
+
+	def euclidean_distance(self, p1, p2):
+		"""Euclidean distance between two points.
+
+		Parameters
+		----------
+		p1 : int
+			Start point.
+		p2 : int 
+			End point.
+
+		Returns
+		-------
+		float
+			Euclidean distance metric.
+		"""
+		return  math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+
+	def nearest_neighbor(self, graph, x_rand, k=2):
+		"""Returns the index of the nearest neighbor.
+		
+		The nearest neighbor from all the nodes in the graph
+		to the randomly generated node.
+
+		Parameters
+		----------
+		graph : list
+			Graph containing all the coordinate nodes.
+		x_rand : tuple 
+			Coordinate of the random node generated.
+		k : int
+			Number of the closest neighbors to examine for each configuration.
+
+		Returns
+		-------
+		tuple
+			Nearest node to the random node generated.	
+		"""
+		distances = []
+		near = []
+
+		for state in graph:
+			distance = self.euclidean_distance(state, x_rand)
+			distances.append(distance)
+
+		# Index of the minimum distance to the generated random node
+		self.min_distance = np.argmin(distances) 
+		x_near = graph[self.min_distance]
+
+		# Indices of the k-smallest distances 
+		distances = np.asarray(distances.copy())
+		min_distances = np.argpartition(distances, k)
+
+		# Get the k-smallest values from the graph
+		for i in range(k):
+			near.append(graph[min_distances[i]])
+
+		return near
+
+	def draw_random_node(self, map_):
+		"""Draws the x_rand node."""
+		pygame.draw.circle(surface=map_, color=self.GREEN, 
+			center=self.x_rand, radius=3)
